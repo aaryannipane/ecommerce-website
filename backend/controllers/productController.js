01ccfdfd180041d4d93700a566418ec8508dc06b
aaryan nipane <nipaneeducation@gmail.com>
2022-07-10 01:11:00 +0530

    another

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,39 @@
+import ProductModel from "../models/productModel.js";
+
 class ProductController{
-    static getAllProduct = (req, res)=>{
-        res.status(200).json({message:"Route is working fine"});
+    
+    // create product --admin
+    static createProduct = async (req, res, next)=>{
+        const product = await ProductModel.create(req.body);
+
+        res.status(201).json({success:true, product})
     }
+
+    static getAllProduct = async (req, res)=>{
+        const products = await ProductModel.find();
+
+        res.status(200).json({success:true, products});
+    }
+
+    // update product --admin
+    static updateProduct = async (req, res, next)=>{
+        let product = await ProductModel.findById(req.params.id);
+
+        if(!product){
+            return res.status(500).json({
+                success:false,
+                message:"Product not found"
+            })
+        }
+
+        product = await ProductModel.findByIdAndUpdate(req.params.id, req.body, {new:true, runValidators:true, useFindAndModify:false});
+
+        res.status(200).json({
+            success:true,
+            product
+        })
+    }
+
 }
 
 
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -4,5 +4,7 @@ import ProductController from "../controllers/productController.js";
 const router = express.Router();
 
 router.get("/products", ProductController.getAllProduct);
+router.post("/product/new", ProductController.createProduct);
+router.put("/product/:id", ProductController.updateProduct);
 
 export default router;
